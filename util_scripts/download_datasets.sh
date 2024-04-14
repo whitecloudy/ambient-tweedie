cbb9076468f616dae40190af02f00d45ef5f6cd2
Giannis Daras <daras.giannhs@gmail.com>
2024-04-13 21:43:47 -0500

    Add LAION-10k and FFHQ to download datasets script

--- util_scripts/download_datasets.sh
+++ util_scripts/download_datasets.sh
@@ -1,11 +1,24 @@
-# Move to the folder you want to download your datasets
-mkdir -p download_scripts
-cd download_scripts
+mkdir -p $BASE_PATH/datasets/  # create datasets folder if it does not exist
+cd $BASE_PATH/datasets  # move to datasets folder
 
-# Script that helps to download AFHQ, CelebA-HQ
-wget https://github.com/clovaai/stargan-v2/raw/master/download.sh
+
+# ------- LAION-10k subset --------- #
+gdown --id 1lUUzhq9yK1YOWJePm12GeJgsUtCfpm_o  # images
+gdown --id 1svzxtGgJRtCyWNW6pQPNGSDkhuDifTzn  # captions
+unzip train.zip # unzip LAION images
+mkdir -p laion-10k # create dir
+mv train/images_large/* laion-10k/ # move to appropriate folder
+mv 
+rm -rf train/ # clean files
+rm -rf train.zip # clean files
+
 
 # ------- CelebA-HQ --------- #
+mkdir -p celeba_hq
+mkdir -p download_scripts
+cd download_scripts
+wget https://github.com/clovaai/stargan-v2/raw/master/download.sh
+
 bash download.sh celeba-hq-dataset
 mkdir -p celeba_hq_train_split
 mv data/celeba_hq/train/male/* celeba_hq_train_split/
@@ -14,6 +27,14 @@ mkdir -p celeba_hq_eval_split
 mv data/celeba_hq/val/male/* celeba_hq_eval_split/
 mv data/celeba_hq/val/female/* celeba_hq_eval_split/
 rm -rf data/
+mv celeba_hq_train_split ../celeba_hq/
+mv celeba_hq_eval_split ../celeba_hq
+cd ..
 
+
+# ------- FFHQ --------- #
+# download ffhq from huggingface
+wget https://huggingface.co/datasets/yangtao9009/FFHQ1024/resolve/main/FFHQ-1024-1.zip?download=true
+wget https://huggingface.co/datasets/yangtao9009/FFHQ1024/resolve/main/FFHQ-1024-2.zip?download=true
 
 
